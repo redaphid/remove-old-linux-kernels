52dd4134fd20a0bed8ee69dab521d85d99fc147c
redaphid <iam@aaronherres.com>
2017-11-12 19:17:26 -0700

    it works. Also autoremove purges without confirmation

--- remove-old-linux-kernels.sh
+++ remove-old-linux-kernels.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-dpkg -l | tail -n +6 | grep -E 'linux-image-[0-9]+' | grep -Fv $(uname -r) | awk '{print $2}'
+OLD_KERNELS=`dpkg -l | tail -n +6 | grep -E 'linux-image-[0-9]+' | grep -Fv $(uname -r) | awk '{print $2}'`
+
+for kernel in $OLD_KERNELS; do
+    apt-get remove -y --purge $kernel
+done
+
+apt-get autoremove -y --purge
